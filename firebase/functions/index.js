cad680858460f12aeee1b6d7ebcf93da78cd9bef
dinosoudi <90942593+dinosoudi@users.noreply.github.com>
2024-06-14 20:10:42 -0600

    HelloWorld Document
    Co-Authored-By: Marlonep <82923825+marlonep@users.noreply.github.com>

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,1 +1,19 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addHelloWorld = functions.https.onRequest(async (req, res) => {
+    const firestore = admin.firestore();
+    try {
+        await firestore.collection('messages').add({
+            message: 'Hello world',
+            timestamp: admin.firestore.FieldValue.serverTimestamp()
+        });
+        res.status(200).send('Document added successfully');
+    } catch (error) {
+        console.error("Error adding document: ", error);
+        res.status(500).send('Error adding document');
+    }
+});
+
 
